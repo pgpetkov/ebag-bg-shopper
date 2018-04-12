d94cd66a19f1385e83817e53f67fd173191d207a
Petar <petkov.generali@gmail.com>
2018-04-12 21:50:43 +0300

    Imported required selenium functions

--- shopper/scraper.py
+++ shopper/scraper.py
@@ -13,6 +13,12 @@ import datetime
 
 from tqdm import tqdm
 
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 db_path = os.path.join(ROOT_DIR, 'db.sqlite3')
 sqlite_file = os.path.join(db_path)
