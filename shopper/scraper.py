ae39bf189adc1fa1a7e02453a4466ea153d5fd17
Petar <petkov.generali@gmail.com>
2018-04-14 10:00:09 +0300

    Added OS check fore firefox driver

--- shopper/scraper.py
+++ shopper/scraper.py
@@ -2,7 +2,7 @@
 
 ######################################################
 #
-# shopper - check for deals on your shopping preferences
+# shopper - check for deals based on your shopping history
 # written by Petar Petkov (petkov.generali@gmail.com)
 #
 ######################################################
@@ -11,6 +11,7 @@ import os
 import sys
 import sqlite3
 import datetime
+from sys import platform
 
 from tqdm import tqdm
 
@@ -26,23 +27,35 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 from selenium.common.exceptions import NoSuchElementException
 
-import win32com.client
+
 
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+if platform == "linux":
+	print("Linux")
+	FFdriver = ROOT_DIR + r"\firefoxdriver"
+elif platform == "darwin":
+	print("Mac")
+	FFdriver = ROOT_DIR + r"\firefoxdriver"
+elif platform == "win32" or "cygwin":
+	print("Windows")
+	FFdriver = ROOT_DIR + r"\firefoxdriver"
+else:
+	print("Can't identify os")
+
+
+
+
 db_path = os.path.join(ROOT_DIR, 'db.sqlite3')
 sqlite_file = os.path.join(db_path)
 conn = sqlite3.connect(sqlite_file)
 c = conn.cursor()
 
 prefs = {"download.default_directory" : ROOT_DIR}
+
 FFdriver = ROOT_DIR + r"\firefoxdriver.exe"
 profile = FirefoxProfile()
-
-profile.set_preference("browser.download.folderList",2)
-profile.set_preference("browser.download.manager.showWhenStarting",False)
-profile.set_preference("browser.download.dir", ROOT_DIR)
-profile.set_preference('browser.helperApps.neverAsk.saveToDisk', "text/plain, application/vnd.ms-excel, text/csv, application/csv, text/comma-separated-values, application/download, application/octet-stream, binary/octet-stream, application/binary, application/x-unknown")
 
 binary = FirefoxBinary('C:\\Program Files\\Mozilla Firefox\\firefox.exe', log_file=sys.stdout)
 binary2 = FirefoxBinary('C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe', log_file=sys.stdout)
@@ -54,4 +67,34 @@ except:
     browser = webdriver.Firefox(executable_path=FFdriver,firefox_profile=profile,firefox_binary=binary2)
 
 
+
+
+
+
+
 print("Hello World!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
